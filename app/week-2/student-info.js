70141f8cc59900cdce695290b866c0165f8bb10b
MEET PATEL <148184953+GitNinja-Meet@users.noreply.github.com>
2024-09-24 02:20:43 -0600

    Week-2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Meet Patel</h1>
+      <p>
+        <Link href="https://github.com/GitNinja-Meet">
+        https://github.com/GitNinja-Meet
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default StudentInfo;
