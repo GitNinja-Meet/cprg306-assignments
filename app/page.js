2bc8b15c43999a9bed0c4d53b54b9bc31522dda9
MEET PATEL <148184953+GitNinja-Meet@users.noreply.github.com>
2024-10-01 17:20:04 -0600

    week-3 Assignment

--- app/page.js
+++ app/page.js
@@ -1,8 +1,11 @@
-export default function Home() {
+export default function Page() {
   return (
-    <div>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <a href="/week-2">Week-2 Assignment</a>
-    </div>
+      <div>
+          <h1> CPRG 306: Web Development 2 - Assignments</h1>
+          <ul>
+              <li><a href="week-2">Week 2</a></li>
+              <li><a href="week-3">Week 3</a></li>
+          </ul>
+      </div>
   );
 }
